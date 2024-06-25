77ffab8a7ab8e00f52ad68ea2968e932ad10eeaf
rajibjonchhen <rajib.jonchhen@gmail.com>
2024-06-25 14:07:33 +0100

    rock paper scissor game completed
    display message in each round and update the score accordingly and the one who scores 3 point wins the game. and replay button appears to replay

--- game.js
+++ game.js
@@ -32,9 +32,50 @@ function getRandomComputerResult() {
   const playerScoreSpanElement = document.getElementById("player-score");
   const computerScoreSpanElement = document.getElementById("computer-score");
   const roundResultsMsg = document.getElementById("results-msg");
+  const winnerMsgElement = document.getElementById("winner-msg");
+  const optionsContainer = document.querySelector(".options-container");
+  const resetGameBtn = document.getElementById("reset-game-btn");
   
   function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    computerScoreSpanElement.innerText = computerScore;
+    playerScoreSpanElement.innerText = playerScore;
+  
+    if (playerScore === 3 || computerScore === 3) {
+      winnerMsgElement.innerText = `${
+        playerScore === 3 ? "Player" : "Computer"
+      } has won the game!`;
+  
+      resetGameBtn.style.display = "block";
+      optionsContainer.style.display = "none";
+    }
   
   };
+  function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = "0";
+    computerScoreSpanElement.innerText = "0";
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = ""
+  };
   
-  showResults("Rock");
+  resetGameBtn.addEventListener("click", resetGame);
+  
+  const rockBtn = document.getElementById("rock-btn");
+  const paperBtn = document.getElementById("paper-btn");
+  const scissorsBtn = document.getElementById("scissors-btn");
+  
+  rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+  });
+  
+  paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+  });
+  
+  scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+  });
