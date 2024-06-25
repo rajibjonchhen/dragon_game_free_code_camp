87aa0c460274d9456c631a651ee39cae538286c7
rajibjonchhen <rajib.jonchhen@gmail.com>
2024-06-25 23:24:49 +0100

    song title and artist name displayed on the board dynamically

--- music.js
+++ music.js
@@ -98,7 +98,8 @@ const playSong = (id) => {
   userData.currentSong = song;
   playButton.classList.add("playing");
 
-  highlightCurrentSong()
+  highlightCurrentSong();
+  setPlayerDisplay()
   audio.play();
 };
 
@@ -128,6 +129,16 @@ const playPreviousSong = () => {
 
     playSong(previousSong.id);
    }
+};
+
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
 const highlightCurrentSong = () => {
