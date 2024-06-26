0b7ab1149850f71622af41cd85770cf52f3eba75
rajibjonchhen <rajib.jonchhen@gmail.com>
2024-06-26 22:37:07 +0100

    shuffle button shuffle the songs and rerender , play button accessible text added

--- music.js
+++ music.js
@@ -99,7 +99,8 @@ const playSong = (id) => {
   playButton.classList.add("playing");
 
   highlightCurrentSong();
-  setPlayerDisplay()
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
   audio.play();
 };
 
@@ -130,6 +131,19 @@ const playPreviousSong = () => {
     playSong(previousSong.id);
    }
 };
+
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+};
+
+
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
@@ -176,6 +190,15 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+const setPlayButtonAccessibleText = () => {
+  const song = userData?.currentSong || userData?.songs[0];
+
+  playButton.setAttribute(
+    "aria-label",
+    song?.title ? `Play ${song.title}` : "Play"
+  );
+};
+
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
@@ -191,6 +214,8 @@ pauseButton.addEventListener("click",  pauseSong);
 nextButton.addEventListener("click", playNextSong);
 
 previousButton.addEventListener("click", playPreviousSong);
+
+shuffleButton.addEventListener("click", shuffle);
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
