395a62bb9e000adc87ac4c9d6691332b03d3269b
rajibjonchhen <rajib.jonchhen@gmail.com>
2024-07-03 01:31:05 +0100

    Update number_converter.js
    recurring function used to convert the decimal to binary

--- number_converter.js
+++ number_converter.js
@@ -2,16 +2,28 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  if (input === 0) {
+    return "0";
+  } else if (input === 1) {
+    return "1";
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
 const checkUserInput = () => {
   if (
     !numberInput.value ||
     isNaN(parseInt(numberInput.value)) ||
     parseInt(numberInput.value) < 0
   ) {
-alert("Please provide a decimal number greater than or equal to 0")
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
   }
 
-  console.log(numberInput.value);
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
