a07ccb4a2a4164bdf75e5fa555c3cd4fb890a335
rajibjonchhen <rajib.jonchhen@gmail.com>
2024-07-16 15:38:55 +0100

    spread sheet with the basic functionality
    can do sum, average, mean, median, get even, random

--- spread_sheet.js
+++ spread_sheet.js
@@ -5,7 +5,13 @@ const infixToFunction = {
     "/": (x, y) => x / y,
   }
   
+  const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
   
+  const highPrecedence = str => {
+    const regex = /([\d.]+)([*\/])([\d.]+)/;
+    const str2 = infixEval(str, regex);
+    return str === str2 ? str : highPrecedence(str2);
+  }
   
   const isEven = num => num % 2 === 0;
   const sum = nums => nums.reduce((acc, el) => acc + el, 0);
@@ -21,9 +27,31 @@ const infixToFunction = {
   }
   
   const spreadsheetFunctions = {
+    
     sum,
     average,
-    median
+    median,
+    even: nums => nums.filter(isEven),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    firsttwo: nums => nums.slice(0, 2),
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: nums => range(...nums),
+    nodupes: nums => [...new Set(nums).values()],
+    "":arg => arg
+  }
+  
+  const applyFunction = str => {
+    const noHigh = highPrecedence(str);
+    const infix = /([\d.]+)([+-])([\d.]+)/;
+    const str2 = infixEval(noHigh, infix);
+    const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+    const toNumberList = args => args.split(",").map(parseFloat);
+    const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+    return str2.replace(functionCall, (match, fn, args) => spreadsheetFunctions.hasOwnProperty(fn.toLowerCase()) ? apply(fn, args) : match);
   }
   
   const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
@@ -38,6 +66,8 @@ const infixToFunction = {
     const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
     const cellRegex = /[A-J][1-9][0-9]?/gi;
     const cellExpanded = rangeExpanded.replace(cellRegex, match => idToText(match.toUpperCase()));
+    const functionExpanded = applyFunction(cellExpanded);
+    return functionExpanded === x ? functionExpanded : evalFormula(functionExpanded, cells);
   }
   
   window.onload = () => {
@@ -67,6 +97,6 @@ const infixToFunction = {
     const element = event.target;
     const value = element.value.replace(/\s/g, "");
     if (!value.includes(element.id) && value.startsWith('=')) {
-  
+      element.value = evalFormula(value.slice(1), Array.from(document.getElementById("container").children));
     }
   }
